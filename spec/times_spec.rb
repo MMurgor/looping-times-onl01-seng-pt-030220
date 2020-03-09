4b321f6540e4ff2872200cb1665015c8152af061
Romena Murgor <romenamurgor@gmail.com>
2020-03-09 18:34:19 +0000

    Automatically backed up by Learn

--- spec/times_spec.rb
+++ spec/times_spec.rb
@@ -1,9 +1,12 @@
 
 .times do using_times 
-	puts "Wingardium Leviosa"
+	puts "Wingardium Leviosa 7 times"
 	"Wingardium Leviosa"
 	"Wingardium Leviosa"
 	"Wingardium Leviosa"
-	
+	"Wingardium Leviosa"
+	"Wingardium Leviosa"
+	"Wingardium Leviosa"
+	"Wingardium Leviosa"
 	end
 end
